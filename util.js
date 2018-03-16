9b6aa2e5a560e6bfdb10b6dae672900278f017d9
cohenmic <35388123+cohenmic@users.noreply.github.com>
2018-03-15 18:45:09 -0700

    Update util.js

--- util.js
+++ util.js
@@ -1,32 +1,34 @@
-function dateValidate(dateIn, forForm){
-	if (dateIn != '0000-00-00'){
-		if(!forForm)
-			return dateFormat(dateIn, 'mm/dd/yyyy');
+module.exports = {
+	dateValidate: function(dateIn, forForm){
+		if (dateIn != '0000-00-00'){
+			if(!forForm)
+				return dateFormat(dateIn, 'mm/dd/yyyy');
+			else
+				return dateFormat(dateIn, 'yyyy-mm-dd');
+		}
 		else
-			return dateFormat(dateIn, 'yyyy-mm-dd');
-	}
-	else
-		return 'undefined';
-}
+			return 'undefined';
+	},
 
-function poundsCalc(weightIn, unitIn){
-	if (!weightIn)
-		return 'undefined';
-	if (unitIn == 0) //if kilos, convert to pounds
-		return (weightIn*2.2).toFixed(0);
-	else
-		return weightIn;
-}
-
-function repsValidate(repsIn, forForm){
-	if (!repsIn){
-		if(forForm)
-			return "";
+	poundsCalc: function(weightIn, unitIn){
+		if (!weightIn)
+			return 'undefined';
+		if (unitIn == 0) //if kilos, convert to pounds
+			return (weightIn*2.2).toFixed(0);
 		else
-			return "N/A";
+			return weightIn;
+	},
+
+	repsValidate: function(repsIn, forForm){
+		if (!repsIn){
+			if(forForm)
+				return "";
+			else
+				return "N/A";
+		}
+		else
+			return repsIn;
 	}
-	else
-		return repsIn;
 }
 /*
 function weightValidate(weightIn, forForm){
